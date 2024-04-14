c237d2b76c775e6d19b91732375b1dabed2c104e
feestjebouwen01 <vandoorenmathijs@gmail.com>
2024-04-14 14:26:13 +0200

    x

---
+++ pages/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Welcome to the Page</h1>
+            <Link href="/app/first_graph">
+                <button type="button">Go to first graph Page</button>
+            </Link>
+        </div>
+    );
+};
+
+export default Page;
